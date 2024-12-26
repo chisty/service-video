0455e9cd02683bc6629090924c707d7b165ba90b
Z Ahmed Chisty <chisty.sust@gmail.com>
2024-12-26 02:15:58 +0100

    commit 20

--- app/services/sales-api/v1/handlers/handlers.go
+++ app/services/sales-api/v1/handlers/handlers.go
@@ -3,12 +3,12 @@ package handlers
 import (
 	"github.com/chisty/service/app/services/sales-api/v1/handlers/hackgrp"
 	v1 "github.com/chisty/service/business/web/v1"
-	"github.com/dimfeld/httptreemux/v5"
+	"github.com/chisty/service/foundation/web"
 )
 
 type Routes struct{}
 
 // Add implements the RouterAdder interface.
-func (Routes) Add(mux *httptreemux.ContextMux, cfg v1.APIMuxConfig) {
-	hackgrp.Routes(mux)
+func (Routes) Add(app *web.App, cfg v1.APIMuxConfig) {
+	hackgrp.Routes(app)
 }
